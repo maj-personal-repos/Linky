33357f990e54ee4764fef8b023adf4b5a626ed78
Miguel Alonso Jr <drmiguelalonsojr@gmail.com>
2015-02-20 12:36:48 -0500

    Added custom segues. Added example.

---
+++ Example/Linky/majSecondViewController.h
@@ -0,0 +1,14 @@
+//
+//  majSecondViewController.h
+//  Linky
+//
+//  Created by Miguel Alonso Jr on 2/20/15.
+//  Copyright (c) 2015 Miguel Alonso Jr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface majSecondViewController : UIViewController
+- (IBAction)close:(id)sender;
+
+@end
